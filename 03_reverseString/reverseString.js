08ef494b9b5ee5c6492afac85a02f2e350f93368
brookej7 <brooke.e.jones@bigpond.com>
2024-03-31 21:07:06 +1000

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("")
+    splitString = splitString.reverse().toString()
+    return splitString.replace(/,/g, "")
 };
 
 // Do not edit below this line
