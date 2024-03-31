d0925921207d005a84a94668d9a0063208281457
brookej7 <brooke.e.jones@bigpond.com>
2024-03-31 21:08:13 +1000

    Added a note on which method would have done whatI originally wanted looking at solution.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
 const reverseString = function(string) {
     let splitString = string.split("")
     splitString = splitString.reverse().toString()
+    //could have used join which removes the , automatically
     return splitString.replace(/,/g, "")
 };
 
