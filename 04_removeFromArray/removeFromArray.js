05aa704f01bc38e1def3f3e0c897be0a851a76db
brookej7 <brooke.e.jones@bigpond.com>
2024-03-31 21:40:27 +1000

    Completed Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeMe) {
+    let position;
+    for (const arg of removeMe) {
+        position = array.indexOf(arg);
+        while (position >= 0 ) {  
+            array.splice(position, 1);
+            position = array.indexOf(arg);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
