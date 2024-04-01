e3b7079fd4e6866642580629156cbddfbefd529a
brookej7 <brooke.e.jones@bigpond.com>
2024-04-01 12:46:37 +1000

    Completed exercise 6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(!Number.isInteger(year)) return false;
+
+    if (year % 4 == 0) {
+
+        let divisbleBy100 = (year % 100 == 0);
+
+        if (divisbleBy100 && (year % 400 == 0)) {
+
+            return true
+
+        } else if (divisbleBy100) {
+            return false;
+        }
+        
+        return true;
+
+    } else {
+
+        return false;
+
+    }
 
 };
 
