f264e98a93ce539af98bc364f85f0791de982ef0
brookej7 <brooke.e.jones@bigpond.com>
2024-04-01 12:34:54 +1000

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (typeof start != 'number' | typeof end != 'number' ) {
+        return "ERROR";
+    }
+    else if (start < 0 | end < 0) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if (end > start) {
+        while (start <= end) {
+            sum += start;
+            start++;
+        }
+    } else {
+        while (end <= start) {
+            sum += end;
+            end++;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
