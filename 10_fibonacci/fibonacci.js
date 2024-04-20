0abfc60ba8b981bb3f5eb4fec2d75a778a629f53
brookej7 <brooke.e.jones@bigpond.com>
2024-04-20 20:39:02 +1000

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(size) {
+    let fib = [1];
+    let start = 1;
+    if (size == 0) {
+        return 0
+    } else if (size < 0) {
+        return "OOPS";
+    }
+    while (fib.length < size) {
+        fib.push(start);
+        start = fib[fib.length - 2] + start;
+    }
+    return fib[size - 1];
 };
 
 // Do not edit below this line
