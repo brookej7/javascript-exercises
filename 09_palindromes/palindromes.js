2ede474cb87baea43314ca53dfdb73dccc5338c9
brookej7 <brooke.e.jones@bigpond.com>
2024-04-20 20:21:01 +1000

    Completed execerise 8

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const lower = word.toLowerCase();
+    const array = lower.split("")
+    let filtered = array.filter((char) => char <= 'z' && char >= '0');
+    const joint = filtered.join("");
+    const size = joint.length;
+    let first = joint.slice(0, size/2).split("").reverse().join("");
+    return joint.endsWith(first);
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
