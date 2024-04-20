09b257aabd3d6a050ec0b29bd3a6e8380b04efcb
brookej7 <brooke.e.jones@bigpond.com>
2024-04-20 21:03:46 +1000

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let dated = people.map((person) => {
+        if (person.yearOfDeath==undefined) {
+            person.yearOfDeath = (new Date()).getFullYear();
+        }
+        return person;
+    }
+    )
+    let sorted = dated.sort((first, next) => (first.yearOfBirth - first.yearOfDeath) > 
+            (next.yearOfBirth - next.yearOfDeath) ? 1:-1);
+    return sorted[0];
 
 };
 
